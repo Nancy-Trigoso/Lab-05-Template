27d42864de4206e70b20ec293a91604800f261fc
Nancy-Trigoso <nancytrigoso1998@gmail.com>
2024-06-18 19:53:49 -0500

    Resolvimos el problema-01

--- js/tests.js
+++ js/tests.js
@@ -10,7 +10,7 @@ function testSum() { //eslint-disable-line
   }
 }
 
-testSum();
+
 
 function testMultiply() { //eslint-disable-line
   if (multiply(5,9)[0] === 45 && multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
