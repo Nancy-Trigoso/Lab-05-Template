5623c2fc7b235c34cb9d366632618713d40aa90a
Nancy-Trigoso <nancytrigoso1998@gmail.com>
2024-06-19 12:40:54 -0500

    Resolvimos el problema-04

--- js/app.js
+++ js/app.js
@@ -88,7 +88,7 @@ function sumArray(sumArr) { //eslint-disable-line
         const resultado = sum(inicio, sumArr[i]);
         inicio = resultado[0];
     }
-    let mensaje = sumArr[0]+","+sumArr[1]+","+sumArr[2] + " was passed in as an array of numbers, and "+ inicio +" is their sum.";
+    let mensaje = sumArr[0] + "," + sumArr[1] + "," + sumArr[2] + " was passed in as an array of numbers, and " + inicio + " is their sum.";
     return[inicio, mensaje];
 }
 
@@ -109,12 +109,15 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
 
+
+function multiplyArray(multArr) { //eslint-disable-line
+    
 }
 
+
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
